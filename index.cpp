0cb1939a5c9c8693516112a9643c5ec5ae3a6065
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-02-27 14:05:24 +0100

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main()
 {
-  int i = 5;
-  cout << "hello" << i << " world!" << endl;
+	int i = 5;
+	cout << "Saisissez une valeur : ";
+	cin >> i;
+	cout << "Hello " << i << " World!" << endl;
 }
