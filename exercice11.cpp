cffba9dd115a58681b37b724a56ae58a95c1febc
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-02-27 15:35:09 +0100

    Update exercice11.cpp

--- exercice11.cpp
+++ exercice11.cpp
@@ -1,8 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <fichier.h>
 
 using namespace std;
 
-int main()
+int main(int argc, char ** argv)
 {
 	int i = 5;
 	cout << "Saisissez une valeur : ";            // Printf
