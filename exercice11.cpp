b94e218b499b766b66f6c4c2abc300bc7bb8a9bf
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-02-27 15:17:49 +0100

    Create exercice11.cpp

---
+++ exercice11.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int i = 5;
+	cout << "Saisissez une valeur : ";            // Printf
+	cin >> i;                                     // Scanf
+	cout << "Hello " << i << " World!" << endl;   // Printf
+
+
+}	
