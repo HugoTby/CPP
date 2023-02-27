79a309fcdd35194e8303ef18e03d810a92450518
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-02-27 14:32:50 +0100

    Create operateurs.cpp

---
+++ operateurs.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int i = 5;
+	cout << "Saisissez une valeur : ";            // Printf
+	cin >> i;                                     // Scanf
+	cout << "Hello " << i << " World!" << endl;   // Printf
+  
+  
+  test(2.0f, round(2.45f));
+  int * ptrCpp = new int[];
+}
